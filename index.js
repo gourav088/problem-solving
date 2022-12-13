97ebe581c58e1d503b59a77a0ac72b13eb97a52e
gourav088 <thakurgourav124421@gmail.com>
2022-12-13 20:33:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// step 1 - store input in a variable 
+
+// step 2 - check if condition for the given letter is vowel or not 
+
+// stpe 3 - if its a vowel so print its a vowel or else print its a consonent .
+
+// step - then console.log to check the result
