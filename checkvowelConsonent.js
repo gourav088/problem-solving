3e7a112be452f23d5a075e6f50a9fa62d9e1be88
gourav088 <thakurgourav124421@gmail.com>
2022-12-14 19:18:36 +0530

    second commit

---
+++ checkvowelConsonent.js
@@ -0,0 +1,9 @@
+/**
+ * Step 1 - Store input in a variable
+ * Step 2 - Check in if condition for the letter is vowel or not
+ * Step 3 - Check If it is vowel
+ * Step 3.1 - Print it is a vowel
+ * Step 4 - Print it is a consonent
+ */
+
+
